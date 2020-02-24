64a07c5a5cc04ed8669409b17f2ea1b371393f13
Karl Taylor <karl_taylor@me.com>
2020-02-24 11:38:23 +0000

    add src

---
+++ src/index.js
@@ -0,0 +1,64 @@
+const axios = require("axios");
+
+const appSlugs = {
+  staging: [
+    "Morelands-Staging-Android",
+    "Morelands-Staging-iOS",
+    "Savving-Staging-Android",
+    "Savvy-Staging-iOS"
+  ],
+  master: []
+};
+
+const allowedBranches = ["staging", "master"];
+
+const getBranch = str => {
+  const toArr = str.split("/");
+  return toArr[toArr.length - 1];
+};
+
+const getUrls = branch => {
+  return appSlugs[branch].map(
+    slug =>
+      `https://api.appcenter.ms/v0.1/apps/WeAreSavvy/${slug}/branches/${branch}/builds`
+  );
+};
+
+exports.handler = async event => {
+  try {
+    const { ref } = event.body;
+
+    const branch = getBranch(ref);
+
+    if (allowedBranches.includes(branch)) {
+      const calls = getUrls(branch).map(call =>
+        axios.post(
+          call,
+          {},
+          {
+            headers: { "X-API-TOKEN": process.env.X_API_TOKEN }
+          }
+        )
+      );
+      const stuff = await Promise.all(calls)
+        .then(data => {
+          console.log("HERE", data);
+          return {
+            statusCode: 200,
+            body: JSON.stringify(data)
+          };
+        })
+        .catch(err => {
+          console.log("Here in error", err);
+          throw new Error(err);
+        });
+      console.log(stuff);
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ ok: true })
+      };
+    }
+  } catch (err) {
+    throw new Error(err);
+  }
+};
